a2983d9b351e5c798871393908103e98f4b853be
Anh Thu <anhthuu1107@gmail.com>
2026-01-19 11:53:13 +0700

    Update FCFS

--- src/algorithms.h
+++ src/algorithms.h
@@ -1,4 +1,13 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 
+#include <vector>
+#include "process.h"
+
+// Compare processes by Arrival Time (for FCFS)
+bool operator<(const Process& P1, const Process& P2);
+
+// FCFS scheduling algorithm
+vector<Process> runFCFS(vector<Process> L);
+
 #endif // ALGORITHMS_H
