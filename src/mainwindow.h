db8541793453ae18c7de40a2c8e7117d334ca5bf
Tu Huynh Thanh <huynhtu30082006@gmail.com>
2026-01-20 09:21:16 +0700

    Update mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <QLabel>
+#include <vector>
+#include "ganttwidget.h"
+#include "list_process.h"
+#include "ganttwidget.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -16,10 +20,42 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
     public:
+            enum class AlgorithmType
+            {
+                NONE,
+                FCFS,
+                SJF
+            };
+
+            // Default state is before run or after remove
+            AlgorithmType current_algo = AlgorithmType::NONE;
+
             MainWindow(QWidget *parent = nullptr);
             ~MainWindow();
+            void button_add_process();
+            void button_remove_process();
+            void button_save_output();
+            void button_load_process();
+            void button_run_fcfs();
+            void button_run_sjf();
+            void button_exit();
+
+            //Helper
+            void showTable(const vector<Process>& L);
+            void showTable();
+            void showAverage(const vector<Process>& L);
+            void gantt_chart(const vector<Process>& L);
+            void clearAllView();
+            void clearResultView();
+            void refresh();
+
     private:
         Ui::MainWindow *ui;
+        QLabel* labelNoData;
+        list_process lp;
+        vector<Process> currentResult;
+        GanttWidget *ganttWidget;
+
 };
 
 
