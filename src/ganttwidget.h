606fa1da16454dd9a8aa663c70c04798b45c21f4
Tu Huynh Thanh <huynhtu30082006@gmail.com>
2026-01-20 09:18:08 +0700

    Update ganttwidget.h

--- src/ganttwidget.h
+++ src/ganttwidget.h
@@ -1,4 +1,25 @@
 #ifndef GANTTWIDGET_H
 #define GANTTWIDGET_H
 
+#include <QWidget>
+#include <QPainter>
+#include <vector>
+#include "process.h"
+
+class GanttWidget : public QWidget
+{
+    Q_OBJECT
+
+    private:
+        vector<Process> processes;
+
+    public:
+        GanttWidget(QWidget* parent = nullptr): QWidget(parent) {}
+        void clear();
+        void setData(const vector<Process>& list);
+        void paintEvent(QPaintEvent*) override;
+
+
+};
+
 #endif // GANTTWIDGET_H
