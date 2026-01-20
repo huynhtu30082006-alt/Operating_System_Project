a249c24f5514576a4f35440453545f905ef7c047
Tu Huynh Thanh <huynhtu30082006@gmail.com>
2026-01-20 09:27:30 +0700

    Update algorithms.h

--- src/algorithms.h
+++ src/algorithms.h
@@ -1,4 +1,12 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
+#include <vector>
+#include "process.h"
+
+bool operator<(const Process& P1, const Process& P2);
+vector<Process> runFCFS(vector<Process> L);                     // Chạy thuật toán FCFS (có Response Time)
+vector<Process> runSJF(vector<Process> L);
+vector<Process> sort_for_show_table(vector<Process>& L);
+vector<Process> sort_for_draw_gantt(vector<Process>& L);
 
 #endif // ALGORITHMS_H
